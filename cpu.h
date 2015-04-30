4398da11ccbb561ef0061839de25530f0a6d124a
crysxd <c.wuerthner@me.com>
2015-04-30 16:21:24 +0800

    Implemented cpu.h

--- cpu.h
+++ cpu.h
@@ -8,5 +8,20 @@
 #define __CPU__
 
 #include <stdio.h>
+#include <inttypes.h>
+#include "types.h"
+
+/* Function pointer for differnt traps */
+trap cpu_page_fault;
+trap cpu_context_switch;
+
+/* Used to indicate a new tick */
+void cpu_tick();
+
+/* Used to run a process on the CPU */
+void cpu_run(memory_trace_t *process, uint64_t *instruction_pointer);
+
+/* Used to preempt the currently running process */
+void cpu_preempt();
 
 #endif
