8f9d78fe36e9a8b787c934ae031c4dab84af8ef5
crysxd <c.wuerthner@me.com>
2015-05-01 09:38:17 +0800

    Added check for argument count

--- control.c
+++ control.c
@@ -11,6 +11,12 @@
 #include "pager.h"
 
 int main(int argc, char **argv) {
+	/* Check argument count */
+	if(argc < 2) {
+		printf("Not enough arguments, usage: "
+			"simulator PATH_TO_PROCESS_TRACE\n");
+		return 1;
+	}
 
 	/* Set traps for cpu */
 	cpu_page_fault = &scheduler_trap_page_fault;
