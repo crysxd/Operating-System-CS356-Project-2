dea3fde2346add5eb400c2f6cf4c11616334921e
crysxd <c.wuerthner@me.com>
2015-05-03 15:20:02 +0800

    Fixed copy error

--- memory.c
+++ memory.c
@@ -7,12 +7,12 @@
 #include "memory.h"
 
 /* Trap if a page fault happens */
-trap cpu_page_fault = NULL
+trap memory_page_fault = NULL;
 
 /* Accesess the memory and raises a page fault if the requested page is not 
    in memory */
 void memory_access(uint32_t address, trap page_fault) {
-	
+
 }
 
 
